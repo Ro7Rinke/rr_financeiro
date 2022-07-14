29b5168706f89f0140540527d6e49ba4dc0a7bb9
Ro7Rinke <ro7rinke@gmail.com>
2022-07-14 03:05:34 -0300

    adicionado dotenv

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -2,8 +2,18 @@ import { isDebug } from "../controller/args"
 import fs from 'fs'
 import Credential from "../type/Credential"
 
-export const credentials:Credential = JSON.parse(<string><unknown>fs.readFileSync(
-    isDebug() 
-        ? `${__dirname}/../database/databaseDev.credentials` 
-        : `${__dirname}/../database/database.credentials`
-))
+import 'dotenv/config'
+
+
+// export const credentials:Credential = JSON.parse(<string><unknown>fs.readFileSync(
+//     isDebug() 
+//         ? `${__dirname}/../database/databaseDev.credentials` 
+//         : `${__dirname}/../database/database.credentials`
+// ))
+
+export const credentials:Credential = JSON.parse(
+    (isDebug()
+        ? process.env.DATABASE_CREDENTIALS_DEV
+        : process.env.DATABASE_CREDENTIALS)
+    || ''
+)
